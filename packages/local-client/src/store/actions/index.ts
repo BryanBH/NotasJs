1b2529f93539eb7c0f96d17be9d7a49000da4977
BryanBH <bryanbenjumea@gmail.com>
2023-07-01 09:14:46 -0400

    action changes

--- packages/local-client/src/store/actions/index.ts
+++ packages/local-client/src/store/actions/index.ts
@@ -4,24 +4,24 @@ import { ActionType } from '../actionTypes';
 export type Direction = 'up' | 'down';
 
 export interface MoveCellAction {
-  type: ActionType.MOVE_CELL;
+  // type: ActionType.MOVE_CELL;
   id: string;
   direction: Direction;
 }
 
 export interface DeleteCellAction {
-  type: ActionType.DELETE_CELL;
+  // type: ActionType.DELETE_CELL;
   info: { id: string };
 }
 
 export interface InsertCellAfterAction {
-  type: ActionType.INSERT_CELL_AFTER;
+  // type: ActionType.INSERT_CELL_AFTER;
   id: string | null;
   cellType: cellType;
 }
 
 export interface UpdateCellAction {
-  type: ActionType.UPDATE_CELL;
+  // type: ActionType.UPDATE_CELL;
   id: string;
   content: string;
 }
